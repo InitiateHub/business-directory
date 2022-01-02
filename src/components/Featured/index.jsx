6827468a0370126b523a81a1dc3834f739e4d08f
Xander Selorm <alexandercubby@gmail.com>
2022-01-02 14:55:46 +0000

    WIP

--- src/components/Featured/index.jsx
+++ src/components/Featured/index.jsx
@@ -3,12 +3,10 @@ import { Grid, Box, Typography } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import SearchBar from 'components/SearchBar';
 import BusinessItem from 'components/BusinessItem';
-import { GetAllBusinesses } from 'services/firebase.storage.service';
 import useStyles from './styles';
 
 const Featured = () => {
   const classes = useStyles();
-  // console.log(GetAllBusinesses());
   return (
     <Grid container className={classes.box}>
       <Grid item xs={false} sm={2} />
